57d30b46e90e59886b7c3dc655a38a710b489c5a
guillaume <gui161295@gmail.com>
2017-11-20 18:11:44 +0100

    Update Entity

--- src/models/drawable/Entity.java
+++ src/models/drawable/Entity.java
@@ -5,4 +5,29 @@ public abstract class Entity {
     private int y;
     private EntityType type;
 
+    public Entity (int x, int y, EntityType type){
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public EntityType getType() {
+        return type;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
